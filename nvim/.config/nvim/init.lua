7a973bcb93825c3c61571a867f8acba84a4b5851
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2022-02-13 16:14:34 +0100

    feat: basic setup for nvim lsp and treesitter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,27 +2,110 @@ require('plugins')
 require('mappings')
 require('general-settings')
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-    files = {'src/parser.c', 'src/scanner.cc'},
-  },
-  filetype = 'org',
-}
+local configs = require'nvim-treesitter.configs'
 
-require'nvim-treesitter.configs'.setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+configs.setup {
+  ensure_installed = "maintained",
   highlight = {
     enable = true,
-    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
   },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+  indent = {
+    enable = true,
+  }
 }
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 require('orgmode').setup({
   org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
   org_default_notes_file = '~/Dropbox/org/refile.org',
 })
+
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+          --workspace = {
+            -- Make the server aware of Neovim runtime files
+            --library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+          --}
+        }
+      }
+    }
+  end
+
+  server:setup(opts)
+end)
+
+  -- Setup nvim-cmp.
+  local cmp = require'cmp'
+
+  cmp.setup({
+    -- snippet = {
+    --   -- REQUIRED - you must specify a snippet engine
+    --   expand = function(args)
+    --     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    --     -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    --     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+    --     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    --   end,
+    -- },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'ultisnips' }
+      -- { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['sumneko_lua'].setup {
+    capabilities = capabilities
+  }
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,31 +1,54 @@
 local packer = require('packer')
 
 packer.startup(function()
-  use {
-    'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'
-  }
-  use  'easymotion/vim-easymotion'
-  use  'preservim/nerdtree'
-  use { 'junegunn/fzf', run = function() vim.fn['fzf#install()'](0) end }
-  use  'junegunn/fzf.vim'
-  use  'morhetz/gruvbox'
+  -- use {
+  --   'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'
+  -- }
+  -- essentials
   use  'tpope/vim-surround'
   use  'tpope/vim-commentary'
+  use  'easymotion/vim-easymotion'
   use  'tpope/vim-fugitive'
   use  'Raimondi/delimitMate'
   use  'tpope/vim-unimpaired'
   use  'mattn/emmet-vim'
   use  'SirVer/ultisnips'
   use  'honza/vim-snippets'
+
+  -- file explorer
+  use  'preservim/nerdtree'
+
+  -- fzf
+  use { 'junegunn/fzf', run = function() vim.fn['fzf#install()'](0) end }
+  use  'junegunn/fzf.vim'
+
+  -- debugging
   use  'puremourning/vimspector'
+
   use  'aklt/plantuml-syntax'
-  use  'tyru/open-browser.vim'
   use  'weirongxu/plantuml-previewer.vim'
+  use  'tyru/open-browser.vim'
+
+  -- colorscheme
+  use  'morhetz/gruvbox'
 
   use {'nvim-treesitter/nvim-treesitter'}
+
   use {'nvim-orgmode/orgmode', config = function()
         require('orgmode').setup{}
     end
   }
+
+  -- LSP
+  use {
+    'neovim/nvim-lspconfig',
+    'williamboman/nvim-lsp-installer',
+  }
+
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 end)
 
