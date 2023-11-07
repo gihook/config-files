97ace3250cf0e066439c4a3b7792dc8d9cead44e
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2023-11-07 08:39:52 +0100

    fix: fix for nvim telescope plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -64,7 +64,7 @@ packer.startup(function()
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
+		tag = " 0.1.4",
 		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
